1e0f3f9f79dbfff5d07c943338cbad691e3c6249
JNOLEN27 <jnolen5075@gmail.com>
2026-04-22 12:17:30 -0400

    Simplify MaintenanceTab addition in main_window.py
    Removed conditional tab addition for MaintenanceTab based on user roles.

--- app/supply_chain_coordination/main_window.py
+++ app/supply_chain_coordination/main_window.py
@@ -136,13 +136,6 @@ class SupplyChainCoordinationWindow(QMainWindow):
         mainttab = MaintenanceTab(self.import_manager, self.userdata)
         tabs.addTab(maintab, "Maintenance")
 
-        from app.utils.config import ADMINUSERS, POWERUSERS
-        username = self.userdata.get('username', '')
-        if username in ADMINUSERS or username in POWERUSERS:
-            from app.supply_chain_coordination.maintenance_tab import MaintenanceTab
-            mainttab = MaintenanceTab(self.import_manager, self.userdata)
-            tabs.addTab(mainttab, "Maintenance")
-
         layout.addWidget(tabs)
  
         self.statusBar().showMessage(f"Logged in as: {self.userdata['username']}")
